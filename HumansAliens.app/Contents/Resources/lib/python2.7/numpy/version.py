e5957060f9f9ef33cf1c368dcef35a33c2c59e66
pedram26 <pedram_764@yahoo.com>
2016-12-23 19:43:42 -0800

    Update application

---
+++ HumansAliens.app/Contents/Resources/lib/python2.7/numpy/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+#
+# To compare versions robustly, use `numpy.lib.NumpyVersion`
+short_version = '1.11.3'
+version = '1.11.3'
+full_version = '1.11.3'
+git_revision = ''
+release = True
+
+if not release:
+    version = full_version
